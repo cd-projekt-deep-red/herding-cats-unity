e77554c67d12a5d371dc13e140fd0469b2a2366a
Eric Jiang <eric.jiang2@gmail.com>
2021-01-28 13:14:36 -0700

    Move player animation logic to LateUpdate.

--- Assets/Scripts/Controllers/PlayerOne.cs
+++ Assets/Scripts/Controllers/PlayerOne.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerOne : MonoBehaviour
 {
@@ -10,10 +8,6 @@ public class PlayerOne : MonoBehaviour
 
     public float playerSpeed = 0f;
 
-    private float maxMovement = .1f;
-
-    //private Vector2 moveVector = new Vector2 { x = 0f, y = 0f };
-    [Range(0f, 1f)] private float movementScaler = .05f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +17,6 @@ public class PlayerOne : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
@@ -37,13 +29,17 @@ public class PlayerOne : MonoBehaviour
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rigidbody.AddForce(movement);
+    }
+
+    private void LateUpdate()
+    {
         playerSpeed = rigidbody.velocity.magnitude;
-        if (playerSpeed > 2 && movement.x > 0)
+        if (playerSpeed > 2 && rigidbody.velocity.x > 0)
         {
             characterAnimator.SetBool("MoveRight", true);
             characterAnimator.SetBool("MoveLeft", false);
         }
-        if (playerSpeed > 2 && movement.x < 0)
+        if (playerSpeed > 2 && rigidbody.velocity.x < 0)
         {
             characterAnimator.SetBool("MoveLeft", true);
             characterAnimator.SetBool("MoveRight", false);
