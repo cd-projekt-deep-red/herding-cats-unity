36fb741b8e19c752f0964bea641301304eb1d4ab
SaldanaWM <Saldana.wm@gmail.com>
2021-01-29 12:45:15 -0700

    add basic goal detector code

---
+++ Assets/Scripts/Controllers/GoalDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalDetector : MonoBehaviour
+{
+    private int catsCaught;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        catsCaught++;
+        Debug.Log($"you have caught {catsCaught} cats");
+    }
+}
