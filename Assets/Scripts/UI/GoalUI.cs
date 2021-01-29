8a0ee9c6183b0f87f55f04ccab9958b80276d303
Caleb Roenigk <calebroegnik@cxw-macpro.lan>
2021-01-29 14:43:35 -0500

    Add Goal and UI elements

---
+++ Assets/Scripts/UI/GoalUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalUI : MonoBehaviour
+{
+
+
+    // Hero the Goal for a set amount of time
+    public void HeroGoal(float duration)
+    {
+      //
+    }
+
+    // Minimize the Goal
+    public void MinimizeGoal()
+    {
+      //
+    }
+}
