da4135f29e0738379eb654d6524f3c7c33b38d88
Caleb Roenigk <calebroegnik@cxw-macpro.lan>
2021-01-30 16:33:49 -0500

    Add shop UI animatior

---
+++ Assets/ShopScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShopScript : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI shopkeeperText;
+    [SerializeField]private string[] shopkeeperDialog;
+
+    public void SetShopMessage(int messageIndex)
+    {
+      shopkeeperText.text = "    " + shopkeeperDialog[messageIndex] + "\n/";
+    }
+}
