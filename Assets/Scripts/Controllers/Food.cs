6c7e1a70b10c6683a6c97dcddee7fe96556b6586
Eric Jiang <eric.jiang2@gmail.com>
2021-01-30 22:32:24 -0700

    Allow player to hold food.

---
+++ Assets/Scripts/Controllers/Food.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : Holdable
+{
+    public int amount;
+    public override void OnPickUp()
+    {
+    }
+
+    public override void OnPutDown()
+    {
+    }
+}
