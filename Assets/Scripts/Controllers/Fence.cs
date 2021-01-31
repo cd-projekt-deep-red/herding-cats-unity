3dfe8ddbfb94dc3f39c6e8a56b713309743e6c52
SaldanaWM <Saldana.wm@gmail.com>
2021-01-30 20:46:46 -0700

    add fence interactable

---
+++ Assets/Scripts/Controllers/Fence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fence : Holdable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public override void OnPickUp()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnPutDown()
+    {
+        throw new System.NotImplementedException();
+    }
+}
